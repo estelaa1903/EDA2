c29cb7e8736b022864f14ef722879a4ece7d8ba3
alexs <alex.alastuey01@estudiant.upf.edu>
2023-04-26 09:58:54 +0200

    gjhg

---
+++ cmake-build-debug/CMakeFiles/EDA2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDA2.dir/main.c.obj"
+  "CMakeFiles/EDA2.dir/main.c.obj.d"
+  "EDA2.exe"
+  "EDA2.exe.manifest"
+  "EDA2.pdb"
+  "libEDA2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EDA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
