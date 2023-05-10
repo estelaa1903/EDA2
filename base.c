29bac4978be0470f9c618d07189e1044674d68f5
Estela <estela.callejon01@estudiant.upf.edu>
2023-05-10 08:51:54 +0200

    hoy

--- base.c
+++ base.c
@@ -9,40 +9,40 @@ void new_usuario(){
     int n;
     char usu[MAX_USUARIO], larg[MAX_LENGTH];
     printf("¿Cual es su nombre de usuario? (MAX. 15)");
-    scanf("%s", usu);
+    scanf("%s", &usu);
     strcpy(us.nombre, usu);
 
     printf("¿Cual es su edad?");
-    scanf("%d", n);
+    scanf("%d", &n);
     us.edad=n;
 
     printf("¿Cual es su correo electronico? (MAX. 50)");
-    scanf("%s", larg);
+    scanf("%s", &larg);
     strcpy(us.correo, larg);
 
     printf("¿Cual es su Ciudad? (MAX. 50)");
-    scanf("%s", larg);
+    scanf("%s", &larg);
     strcpy(us.ubi, larg);
 
     printf("Liste 5 preferencias:");
     printf("\nNum. 1:");
-    scanf("%s",larg);
+    scanf("%s",&larg);
     strcpy(us.pref.pref1, larg);
 
     printf("\nNum. 2:");
-    scanf("%s",larg);
+    scanf("%s",&larg);
     strcpy(us.pref.pref2, larg);
 
     printf("\nNum. 3:");
-    scanf("%s",larg);
+    scanf("%s",&larg);
     strcpy(us.pref.pref3, larg);
 
     printf("Num. 4:");
-    scanf("%s",larg);
+    scanf("%s",&larg);
     strcpy(us.pref.pref4, larg);
 
     printf("Num. 5:");
-    scanf("%s",larg);
+    scanf("%s",&larg);
     strcpy(us.pref.pref5, larg);
 
 }
