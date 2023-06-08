2d0539dd3fb753d14858b3ca94a864a355d7f8aa
alexs <alex.alastuey01@estudiant.upf.edu>
2023-06-08 19:11:07 +0200

    funciona diccionario

---
+++ cmake-build-debug/CMakeFiles/EDA2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDA2.dir/main.c.obj"
+  "CMakeFiles/EDA2.dir/main.c.obj.d"
+  "CMakeFiles/EDA2.dir/menu.c.obj"
+  "CMakeFiles/EDA2.dir/menu.c.obj.d"
+  "CMakeFiles/EDA2.dir/users.c.obj"
+  "CMakeFiles/EDA2.dir/users.c.obj.d"
+  "EDA2.exe"
+  "EDA2.exe.manifest"
+  "EDA2.pdb"
+  "libEDA2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EDA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
