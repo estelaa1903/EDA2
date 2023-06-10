df8d5882ba7123c49786d726bf2627788febeb17
Estela <estela.callejon01@estudiant.upf.edu>
2023-06-10 14:43:59 +0200

    :)

---
+++ colores.h
@@ -0,0 +1,22 @@
+//
+// Created by estel on 09/06/2023.
+//
+
+#ifndef EDA2_COLORES_H
+#define EDA2_COLORES_H
+#define BOLD_BLACK "\e[1;90m"
+#define BOLD_YELLOW "\e[1;93m"
+#define BOLD_MAGENTA "\e[1;95m"
+#define BOLD_CYAN "\e[1;96m"
+#define BOLD_WHITE "\e[1;97m"
+#define FONT_RED "\e[31m"
+#define FONT_BLUE "\e[34m"
+#define RESET "\e[0m"
+
+
+
+
+#define BOLD BOLD_WHITE
+
+
+#endif //EDA2_COLORES_H
